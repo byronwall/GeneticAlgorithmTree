f93a512fa0813fe3498cf1b1be330c0df69ba2b0
byron wall <byron@byroni.us>
2016-01-25 22:34:06 -0700

    Testing raw data

--- GeneTree/DataPoint.cs
+++ GeneTree/DataPoint.cs
@@ -8,9 +8,30 @@ namespace GeneTree
 {
     public class DataPoint
     {
-        //TODO DataPoint needs to be upgraded to handle various data types instead of double
+		//TODO DataPoint needs to be upgraded to handle various data types instead of double
 
-        public double[] Data;
+		public IEnumerable<string> raw_data;
+        public double?[] Data;
         public string Classification;
+        
+        public void LoadDataFromRaw(IEnumerable<string> data){
+			this.raw_data = data;			
+        }
+        
+        private void ProcessRaw(){
+			//for now, just loop through and put a number in place if possible
+        	
+			Data = new double?[raw_data.Count];
+			
+			int i =0;
+			double temp;
+			foreach (var datum in raw_data) {
+				if(double.TryParse(datum, ref Data[i])){
+					Data[i] = null;
+				}
+			}
+			
+        	return;
+        }
     }
 }
